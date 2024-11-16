127de3940b16a2ec904f75f79666918f3b9419d5
Muhammad Rafly Wirayudha <112853148+raflywirayudha@users.noreply.github.com>
2024-11-16 21:40:35 +0700

    💎 style – adjusted text padding for responsive layout on mobile

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,14 +6,14 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   const handleBackToHome = () => {
-    navigate("/"); // Mengarahkan ke halaman home ("/")
+    navigate("/");
   };
 
   return (
     <div className="flex items-center bg-gradient-to-br from-blue-50 to-white justify-center min-h-screen flex-col">
       <img src={NotFound_logo} alt="404" className="h-80" />
       <h1 className="font-bold text-4xl">Page not found</h1>
-      <h2 className="pt-3 text-sm text-gray-500">
+      <h2 className="pt-3 text-sm text-gray-500 px-10 text-center">
         Oops! Looks like you followed a bad link. If you think this is a problem
         with us, please tell us.
       </h2>
