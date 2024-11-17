8bd22075e0986d54d59a74d0d14e32c1a23b6eb0
Gilang Indra <gilangindra734@gmail.com>
2024-11-17 11:53:43 +0700

    🔥 feat – integrate DashboardLayout with role-based routing for mahasiswa and dosen

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import LandingPage from "./pages/LandingPages";
 import Pendaftaran from "./pages/mahasiswa/Pendaftaran";
 import Status from "../src/pages/mahasiswa/Status";
 import NotFound from "../src/pages/NotFound";
-import DashboardLayoutMahasiswa from "./components/DashboardLayout.jsx";
 import Persyaratan from "./pages/mahasiswa/Persyaratan";
 import PascaSeminar from "./pages/mahasiswa/PascaSeminar.jsx";
 import DashboardInstansi from "./pages/instansi/Dashboard.jsx"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import DashboardDosen from "./pages/dosen/Dashboard.jsx";
+import DashboardLayout from "./components/DashboardLayout.jsx";
 
 const App = () => {
     return (
@@ -18,14 +19,17 @@ const App = () => {
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="*" element={<NotFound/>}/>
-                <Route element={<DashboardLayoutMahasiswa/>}>
+                <Route element={<DashboardLayout role="mahasiswa"/>}>
                     <Route path="/mahasiswa/dashboard" element={<Dashboard/>}/>
                     <Route path="/mahasiswa/pendaftaran" element={<Pendaftaran/>}/>
                     <Route path="/mahasiswa/persyaratan" element={<Persyaratan/>}/>
                     <Route path="/mahasiswa/pasca-seminar" element={<PascaSeminar/>}/>
                     <Route path="/mahasiswa/status" element={<Status/>}/>
                 </Route>
-                <Route path="/pembimbingInstansi/Dashboard" element={<DashboardInstansi/>}/>
+                <Route path="/pembimbing-instansi/Dashboard" element={<DashboardInstansi/>}/>
+                <Route element={<DashboardLayout role="dosen"/>}>
+                    <Route path="/dosen/dashboard" element={<DashboardDosen/>}/>
+                </Route>
             </Routes>
         </Router>
     );
